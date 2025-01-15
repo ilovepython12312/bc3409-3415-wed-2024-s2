d53631ff0cc64d4001f0b336dda34c379f68736f
ilovepython12312 <brendangay2000@gmail.com>
2025-01-15 08:39:32 +0000

    1

--- app.py
+++ app.py
@@ -1,2 +1,11 @@
 from flask import Flask
 from flask import render_template,request
+
+app= Flask(__name__) ##variable is same as file name
+
+@app.route("/")
+def index():
+    return(render_template("index.html"))
+
+if __name__ == "__main__":
+    app.run()
